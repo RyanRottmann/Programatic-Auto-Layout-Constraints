9fbf7c2fd84b6e32b29583ed333bbb4d43cd6bed
Ryan Rottmann <ryanrottmann@Ryans-MacBook-Pro.local>
2019-02-15 21:57:21 -0600

    Bug fixes and added functionality

--- AutoLayoutConstraints/MainViewController.swift
+++ AutoLayoutConstraints/MainViewController.swift
@@ -11,13 +11,15 @@ import UIKit
 class MainViewController: UIViewController {
     let myButton = UIButton()
     let myText = UILabel()
+    
     var labelCons: [NSLayoutConstraint] = []
     var buttonCons: [NSLayoutConstraint] = []
+
+    
     var buttonCount = 0;
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         myButton.translatesAutoresizingMaskIntoConstraints = false
         myButton.backgroundColor = UIColor.orange
@@ -44,36 +46,47 @@ class MainViewController: UIViewController {
         
         labelCons = [textHeight, textWidth, textXPlacement, textYPlacement]
         NSLayoutConstraint.activate(labelCons)
-        
-
 
         buttonCons = [topConstraint, bottomConstraint, leftConstraint, rightConstraint]
         NSLayoutConstraint.activate(buttonCons)
         
         myButton.addTarget(self, action: #selector(action), for: .touchUpInside)//on button press
 
-
         // Do any additional setup after loading the view.
     }
     
     @objc func action(){
         buttonCount += 1
+        
         NSLayoutConstraint.deactivate(buttonCons)
-        let buttonHeight = myButton.heightAnchor.constraint(equalToConstant: 200)
-        let buttonWidth = myButton.widthAnchor.constraint(equalToConstant: 200)
-        let buttonXPlacement = myButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
-        let buttonYPlacement = myButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
-        let newButtonCons: [NSLayoutConstraint] = [buttonHeight, buttonWidth, buttonXPlacement, buttonYPlacement]
+
+        
+        var buttonHeight = myButton.heightAnchor.constraint(equalToConstant: 200)
+        var buttonWidth = myButton.widthAnchor.constraint(equalToConstant: 200)
+        var buttonXPlacement = myButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+        var buttonYPlacement = myButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        
+        buttonCons = [buttonHeight, buttonWidth, buttonXPlacement, buttonYPlacement]
+      
+        NSLayoutConstraint.deactivate(buttonCons)
         NSLayoutConstraint.deactivate(labelCons)
+        
         myText.translatesAutoresizingMaskIntoConstraints = false
+        
         if (buttonCount % 2 == 0){
             myText.backgroundColor = UIColor.yellow
+            //myButton.backgroundColor = UIColor.gray
+
         } else {
             myText.backgroundColor = UIColor.red
+            //myButton.backgroundColor = UIColor.blue
+
         }
         
         myText.text = String(buttonCount)
+        
         self.view.addSubview(myText)
+        
         if (buttonCount % 3 == 0){
             let textYPlacement = myText.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 50)
             let textHeight = myText.heightAnchor.constraint(equalToConstant: 20)
@@ -81,14 +94,32 @@ class MainViewController: UIViewController {
             let textXPlacement = myText.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
             
             self.view .bringSubviewToFront(myText)
+            
             labelCons = [textHeight, textWidth, textXPlacement, textYPlacement]
+            
+            buttonHeight = myButton.heightAnchor.constraint(equalToConstant: 200)
+            buttonWidth = myButton.widthAnchor.constraint(equalToConstant: 100)
+            buttonXPlacement = myButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+            buttonYPlacement = myButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+            
+            buttonCons = [buttonHeight, buttonWidth, buttonXPlacement, buttonYPlacement]
+
         } else if (buttonCount % 3 == 1){
             let textYPlacement = myText.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 150)
             let textLeftConstraint = myText.leadingAnchor.constraint(equalTo: self.view.leadingAnchor)
             let textRightConstraint = myText.rightAnchor.constraint(equalTo: self.view.rightAnchor)
             let textXPlacement = myText.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+            
             self.view .bringSubviewToFront(myText)
+            
             labelCons = [textRightConstraint, textLeftConstraint, textXPlacement, textYPlacement]
+            
+            buttonHeight = myButton.heightAnchor.constraint(equalToConstant: 50)
+            buttonWidth = myButton.widthAnchor.constraint(equalToConstant: 300)
+            buttonXPlacement = myButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+            buttonYPlacement = myButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+            
+            buttonCons = [buttonHeight, buttonWidth, buttonXPlacement, buttonYPlacement]
         } else {
             let textYPlacement = myText.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -20)
             let textHeight = myText.heightAnchor.constraint(equalToConstant: 20)
@@ -97,10 +128,26 @@ class MainViewController: UIViewController {
             
             self.view .bringSubviewToFront(myText)
             labelCons = [textHeight, textWidth, textXPlacement, textYPlacement]
+            
+            buttonHeight = myButton.heightAnchor.constraint(equalToConstant: 400)
+            buttonWidth = myButton.widthAnchor.constraint(equalToConstant: 100)
+            buttonXPlacement = myButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+            buttonYPlacement = myButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+            
+            buttonCons = [buttonHeight, buttonWidth, buttonXPlacement, buttonYPlacement]
         }
+        
+//        buttonHeight = myButton.heightAnchor.constraint(equalToConstant: 200)
+//        buttonWidth = myButton.widthAnchor.constraint(equalToConstant: 200)
+//        buttonXPlacement = myButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+//        buttonYPlacement = myButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+//
+//        buttonCons = [buttonHeight, buttonWidth, buttonXPlacement, buttonYPlacement]
 
+        self.view.bringSubviewToFront(myText)
+        
         NSLayoutConstraint.activate(labelCons)
-        NSLayoutConstraint.activate(newButtonCons)
+        NSLayoutConstraint.activate(buttonCons)
         
     }
 
